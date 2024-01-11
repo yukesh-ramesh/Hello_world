dbba305bb862f180f4940ff7ef0db4b4ea7ec65c
yukesh-ramesh <yukeshyuky1997@gmail.com>
2024-01-11 05:09:52 -0500

    first python code

---
+++ hello.py
@@ -0,0 +1,11 @@
+1.def factorial (n):
+2.    if n == 0:
+3.        return :
+4.    else:
+5.       return n * factorial(n-1)
+6.
+7.number = 5
+8.result = factorial(number)
+9.print(f"The factorial of (number) is (result).")
+
+
